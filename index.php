d8dae8a589d9f861a8e8e0e85474b40a3263592b
Rostazore <11044833+Rostazore@users.noreply.github.com>
2017-11-03 15:29:41 +0100

    Autres notions PHP

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
             $nom_reptilien = "Vetu";
             $prénom_reptilien = "Guillaume";
             $âge_reptilien = "20";
+            $santé_reptilien = 100;
+            $santé_mort = 0;
 
             echo "Bonjour, je vais tuer le reptilien " . $prénom_reptilien . " " . $nom_reptilien . " ! ";
             echo "<br/>";
@@ -30,8 +32,43 @@
                 echo "enfant. ";
             }
             echo "<br/>";
+            echo "Il adore manger du chien. ";
+            echo "<br/>";
 
+            echo $santé_reptilien <=> $santé_mort, "<br />1 <=> 2 : " ,1 <=> 2, "<br />2 <=> 1 : ", 2 <=> 1;
+            echo "<br />";
 
+            // Déclaration de variables
+            $a = null;
+            $b = 2;
+            $c = 5;
+            $d;
+
+            echo '$a=null, $b=2, $c=5, $d  =>  $a ?? $b ?? $c ?? $d : ', $a ?? $b ?? $c ?? $d;
+            echo "<br/>";
+
+            // L'arithmétique
+            $a = 5;
+            $b = $a + 5;
+            echo "a vaut $a, b vaut $b<br />";
+
+            $b = $a += 5;
+            echo "a vaut $a, b vaut $b<br />";
+
+            echo '++$a vaut ', ++$a, ' , $a++ vaut ', $a++,  ' , $a vaut ', $a, "<br />";
+            echo "<pre>Ce code n'est pas interprété…\n…du tout</pre>";
+            echo "<pre>";
+            print_r ($a);
+            echo "</pre>";
+            echo "<pre>";
+            var_dump ($a);
+            echo "</pre>";
+            $tableau = ["couleur" => "rouge", "taille" => 1.70];
+            echo "<pre>";
+            var_dump ($tableau);
+            echo "</pre>";
+
+            die("fin");
         ?>
     </body>
 </html>
