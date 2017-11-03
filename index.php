2452bed3e3372cc4f263131f5d6f2b1328406118
Rostazore <11044833+Rostazore@users.noreply.github.com>
2017-11-03 12:42:32 +0100

    Correction du titre et ajout.

--- index.php
+++ index.php
@@ -4,11 +4,33 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Tuons du reptiliens !</title>
+        <title>Tuons du reptilien !</title>
     </head>
     <body>
         <?php
-            echo "Bonjour, je vais tuer du reptilien !";
+            $nom_reptilien = "Vetu";
+            $prénom_reptilien = "Guillaume";
+            $âge_reptilien = "20";
+
+            echo "Bonjour, je vais tuer le reptilien " . $prénom_reptilien . " " . $nom_reptilien . " ! ";
+            echo "<br/>";
+            echo "Il n'est pas vieux, il a $âge_reptilien ans. ";
+            echo "<br/>";
+            echo "Il est ";
+            if ($âge_reptilien >= 18)
+            {
+                echo "majeur. ";
+            }
+            elseif ($âge_reptilien > 15) 
+            {
+                echo "ado. ";
+            }
+            else
+            {
+                echo "enfant. ";
+            }
+            echo "<br/>";
+
 
         ?>
     </body>
